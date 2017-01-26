d6a4fb64b588e7d2ac0cc798d0b0bd6fa4025b80
Antoine Cusset <a.cusset@gmail.com>
2017-01-26 22:21:26 +0100

    create product list hashmap from api response

--- app/src/main/java/com/esgi/iw3/g26/shoppinglist/ProductsActivity.java
+++ app/src/main/java/com/esgi/iw3/g26/shoppinglist/ProductsActivity.java
@@ -8,9 +8,17 @@ import android.widget.TextView;
 
 import com.esgi.iw3.g26.shoppinglist.AsyncTask.ProductTask.ProductListTask;
 import com.esgi.iw3.g26.shoppinglist.AsyncTask.ShoppingListTask.ShoppingListListTask;
+import com.esgi.iw3.g26.shoppinglist.Entity.Product;
+import com.esgi.iw3.g26.shoppinglist.Entity.ShoppingList;
 import com.esgi.iw3.g26.shoppinglist.Interface.IHttpRequestListener;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Jolan on 24/01/2017.
@@ -21,6 +29,8 @@ public class ProductsActivity extends ListActivity implements IHttpRequestListen
     TextView content;
     ListView listView;
     private ProductListTask listProductTask = null;
+    private UserSession session;
+    private List<HashMap<String, String>> products = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +38,10 @@ public class ProductsActivity extends ListActivity implements IHttpRequestListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
 
+        this.session = new UserSession(getApplicationContext());
 //        listView = (ListView) findViewById(R.id.list);
 
-        String token = "123145644897";
+        String token = session.getToken();
         String listId = "12" ;
 
         listProductTask = new ProductListTask(token, listId);
@@ -50,7 +61,17 @@ public class ProductsActivity extends ListActivity implements IHttpRequestListen
 
     @Override
     public void onSuccess(JSONObject object) {
+        this.products.clear();
+        try {
+            JSONArray array = object.getJSONArray("result");
+            for (int i = 0; i < array.length(); i++) {
+                Product product = new Product(array.getJSONObject(i));
+                products.add(product.toHashMap());
+            }
+        } catch (JSONException e) {
 
+        }
+        //TODO les données sont dans l'attribut il faut les charger dans l'adapter
     }
 
     @Override
